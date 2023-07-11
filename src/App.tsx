7c8d00ad58e7f49dd69af95f5328b837bb6d5a33
Alvaro <alvaromagu2002@gmail.com>
2023-07-11 14:00:40 +0200

    install and use background removal

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,68 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import { DropImageInput } from './components/drop-image/drop-image-input'
 import { TwoUp } from './components/two-up/two-up'
+import removeBackground, { type Config } from '@imgly/background-removal'
+
+const fileAsUrl = async (file: File | Blob): Promise<string> => {
+  return await new Promise((resolve, reject) => {
+    const reader = new FileReader()
+    reader.onload = () => resolve(reader.result as string)
+    reader.onerror = () => reject(reader.error)
+    reader.readAsDataURL(file)
+  })
+}
 
 function App () {
   const [filePath, setFilePath] = useState<string | null>(null)
+  const [removedBgPath, setRemovedBgPath] = useState<string | null>(null)
   const [loading, setLoading] = useState(false)
-
-  function handleChange (file: File): void {
-    const fr = new FileReader()
-    fr.onload = () => {
-      setFilePath(fr.result as string)
-      setLoading(false)
+  const [model, setModel] = useState<'small' | 'medium'>('small')
+  const removeBgConfig: Config = useMemo(() => {
+    return {
+      model,
+      progress: (key: string, current: number, total: number) => {
+        console.log(`Progress ${key}: ${current}/${total}`)
+      }
     }
+  }, [model])
+
+  function handleFileChange (file: File): void {
     setLoading(true)
-    setTimeout(() => fr.readAsDataURL(file), 2000)
+    console.log(removeBgConfig)
+    removeBackground(file, removeBgConfig)
+      .then(async res => {
+        console.log('removed bg')
+        return await Promise.all([
+          fileAsUrl(file),
+          fileAsUrl(res)
+        ])
+      })
+      .then(([fileUrl, removedBgUrl]) => {
+        console.log('got urls')
+        setFilePath(fileUrl)
+        setRemovedBgPath(removedBgUrl)
+      })
+      .catch(err => console.log(err))
+      .finally(() => setLoading(false))
   }
 
   return (
     <main className='max-w-2xl h-full m-auto relative pt-10'>
-      <header className='fixed top-0 inset-x-0 max-w-2xl m-auto bg-zinc-900 border-b'>
+      <header className='fixed top-0 inset-x-0 max-w-2xl m-auto bg-zinc-900 border-b flex justify-between items-center'>
         <h1 className='text-xl font-semibold'>Background Remover</h1>
+        <div className='flex gap-3 items-center'>
+          <label htmlFor='model' className='text-sm font-semibold'>
+            Model:
+          </label>
+          <select id='model' onChange={evt => setModel(evt.target.value as 'small' | 'medium')} defaultValue='small' className='border text-sm rounded-lg block bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500'>
+            <option value='small'>Small</option>
+            <option value='medium'>Medium</option>
+          </select>
+        </div>
       </header>
       <section className='h-full grid place-content-center'>
         <DropImageInput
-          handleChange={handleChange}
+          handleChange={handleFileChange}
           loading={loading}
           LoadingPlaceholder={
             <div className='flex flex-col items-center justify-center gap-2'>
@@ -34,7 +73,7 @@ function App () {
           ImagePreview={
             <TwoUp
               firstChild={<img src={filePath ?? ''} />}
-              secondChild={<img src={filePath ?? ''} />}
+              secondChild={<img src={removedBgPath ?? ''} />}
             />
           }
         />
--- src/components/drop-image/drop-image-input.tsx
+++ src/components/drop-image/drop-image-input.tsx
@@ -88,7 +88,7 @@ export function DropImageInput ({
         className='text-red-700 hover:text-red-400 transition-colors absolute -top-2 -right-2 bg-zinc-950 rounded-full p-1'
         onClick={removeFile}
       >
-        <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' stroke-width='2' stroke='currentColor' fill='none' strokeLinecap='round' strokeLinejoin='round'>
+        <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' strokeWidth='2' stroke='currentColor' fill='none' strokeLinecap='round' strokeLinejoin='round'>
           <path stroke='none' d='M0 0h24v24H0z' fill='none' />
           <path d='M18 6l-12 12' />
           <path d='M6 6l12 12' />
